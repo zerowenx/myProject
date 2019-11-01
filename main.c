8905f011bebbe3a23655e4d97f163a057e071146
zerowen <wenx.zero@gmail.com>
2019-11-02 07:31:44 +0800

    add fun

--- main.c
+++ main.c
@@ -1,5 +1,16 @@
 #include <studio.h>
 
+typedef int(*FunType)(int, int);
+
+int sum(int a, int b)
+{
+    int sum = 0;
+    sum = a + b;
+    print("lwtest sum :%d\n", sum);
+    return sum;
+}
+
+
 int main()
 {
     print("lwtest hello world.\n");
